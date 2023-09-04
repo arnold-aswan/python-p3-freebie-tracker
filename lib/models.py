0a1a24a7d565e3199ae6121fe588a15cfbfe002e
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-09-04 10:52:39 +0300

    completed assignment

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Table, create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 convention = {
@@ -9,12 +9,27 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+company_dev = Table(
+    'company_dev',
+    Base.metadata,
+    Column('company_id', ForeignKey('companies.id'), primary_key=True),
+    Column('dev_id', ForeignKey('devs.id'), primary_key=True),
+    extend_existing=True,
+)
+
 class Company(Base):
     __tablename__ = 'companies'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
+    
+    devs = relationship('Dev', secondary='company_dev', back_populates='companies')
+    freebies = relationship('Freebie', backref=backref('company'), cascade='all, delete-orphan')
+
+    def __init__(self, name, founding_year):
+        self.name = name
+        self.founding_year = founding_year
 
     def __repr__(self):
         return f'<Company {self.name}>'
@@ -24,6 +39,68 @@ class Dev(Base):
 
     id = Column(Integer(), primary_key=True)
     name= Column(String())
+    
+    companies = relationship('Company', secondary='company_dev', back_populates='devs')
+    freebies = relationship('Freebie', backref=backref('dev'), cascade='all, delete-orphan')
+
+    def __init__(self, name):
+        self.name = name
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+    
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    
+    def __init__(self, item_name, value, company_id, dev_id):
+        self.item_name = item_name
+        self.value = value
+        self.company_id = company_id
+        self.dev_id = dev_id
+    
+    def __repr__(self):
+        return f'Freebie(id={self.id}, ' + \
+            f'item_name={self.item_name}, ' + \
+            f'value={self.value})'
+         
+         
+# engine = create_engine('sqlite:///freebies.db')            
+# Base.metadata.create_all(engine)  
+
+# Session = sessionmaker(bind=engine)
+# session = Session()          
+
+#  add company
+# name = 'optimum'
+# founding_year = 2005
+# comp = Company(name, founding_year)
+# session.add(comp)
+# session.commit()
+# print('company added')
+
+# add dev
+# name = 'daniel'
+
+# dev = Dev(name)
+# session.add(dev)
+# session.commit()
+# print("dev added")
+
+# comp.devs.append(dev)
+
+
+# add freebie
+# item = 'hoodie'
+# value = 68
+# ci =1
+# di = 1
+# free = Freebie(item, value, ci, di)
+# session.add(free)
+# session.commit()
+# print('Freebie added')
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,83 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+from faker import Faker
+import random
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Company, Dev, Freebie
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///freebies.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    session.query(Company).delete()
+    session.query(Dev).delete()
+    session.query(Freebie).delete()
+    
+    fake = Faker()
+    
+    
+    companies = []
+    for i in range(10):
+        company = Company(
+            name = fake.catch_phrase(),
+            founding_year = fake.year(),
+        )
+        
+        # add and commit individually to get id
+        session.add(company)
+        session.commit()
+        
+        companies.append(company)
+                         
+    devs = [] 
+    for i in range(10):
+        dev = Dev(
+            name = fake.name(),
+        )
+        
+        session.add(dev)
+        session.commit()
+        
+        devs.append(dev)
+        
+        
+    freebies = []
+    for company in companies:
+        for i in range(random.randint(1,5)):
+            dev = random.choice(devs)
+            if company not in dev.companies:
+                dev.companies.append(company)
+                session.add(dev)
+                session.commit()
+                
+            freebie = Freebie(  
+                item_name = fake.word(),
+                value = fake.random_int(min=1, max=65),
+                company_id = random.choice(session.query(Company).all()).id,
+                dev_id = random.choice(session.query(Dev).all()).id 
+            )    
+            
+            freebies.append(freebie)
+            
+    session.bulk_save_objects(freebies)
+    session.commit()
+    session.close()        
+        
+    # for i in range(20):
+    #     freebie = Freebie(
+    #         item_name = fake.word(),
+    #         value = fake.random_int(min=1, max=65),
+    #         company_id = random.choice(session.query(Company).all()).id,
+    #         dev_id = random.choice(session.query(Dev).all()).id
+    #     )    
+        
+    #     session.add(freebie)
+    #     session.commit()
+        
+        
+    # session.commit()
